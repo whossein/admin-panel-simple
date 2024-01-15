6664b6d171b62060f192bfb456eeca7ca633b6fa
h.aghatabar lenovo <whossein@hotmail.com>
2024-01-16 00:04:01 +0330

    first commit, Add create project with vite & antd, tailwind and sass support

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "antd";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <Button> Button </Button>
+    </>
+  );
+}
+
+export default App;
